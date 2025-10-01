95d5cd4988b2aa569c4e16059dfdbba5e6a2daef
juanvalencia <asiquedabien123@gmail.com>
2025-10-01 15:05:19 -0500

    comit 6: añadir imagen de cupcake con espaciado

--- app/src/main/java/com/example/cupcakeapp1/ui/StartOrderScreen.kt
+++ app/src/main/java/com/example/cupcakeapp1/ui/StartOrderScreen.kt
@@ -1,13 +1,19 @@
 package com.example.cupcakeapp1.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cupcakeapp1.R
 import com.example.cupcakeapp1.ui.theme.CupcakeAppTheme
 
@@ -22,11 +28,17 @@ fun StartOrderScreen(
         Column(
             modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(
-                dimensionResource(R.dimen.padding_small)
-            )
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
         ) {
-            // Aquí irán los elementos de la parte superior
+            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_medium)))
+
+            Image(
+                painter = painterResource(R.drawable.cupcake),
+                contentDescription = null,
+                modifier = Modifier.width(300.dp)
+            )
+
+            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_medium)))
         }
     }
 }
